edaadce0116dc699f0a383fb4c775c3461ae7adc
kkkont <116272343+kkkont@users.noreply.github.com>
2025-02-04 13:10:13 +0200

    Initial backend and frontend

---
+++ currency-app-backend/src/main/java/com/currency_app/currency_app_backend/CurrencyAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.currency_app.currency_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyAppBackendApplication.class, args);
+	}
+
+}

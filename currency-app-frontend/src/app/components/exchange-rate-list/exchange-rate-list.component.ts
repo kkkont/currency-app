b7065557cacee0a6f8dbd036f4a8b0073126dc20
kkkont <116272343+kkkont@users.noreply.github.com>
2025-02-05 17:00:33 +0200

    Front page and exchange rates table styling

--- currency-app-frontend/src/app/components/exchange-rate-list/exchange-rate-list.component.ts
+++ currency-app-frontend/src/app/components/exchange-rate-list/exchange-rate-list.component.ts
@@ -5,6 +5,10 @@ import { NgForOf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ExchangeRate } from '../../models/exchange-rate.model';
 import { CommonModule } from '@angular/common';
+import { CurrencyData } from '../../models/currency-data.model';
+import currencyData from '../../../assets/data/currency-data.json';
+import { DatePipe } from '@angular/common';
+
 /**
  * ExchangeRateListComponent fetches and displays the lates exchange rates for different currencies.
  * It allows the user to click on currency to navigate to its detailed history and conversion page.
@@ -17,20 +21,24 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [NgForOf, FormsModule, CommonModule],
   templateUrl: './exchange-rate-list.component.html',
+  providers: [DatePipe],
 })
 export class ExchangeRateListComponent implements OnInit {
   rates: ExchangeRate[] = [];
   loading: boolean = false;
   errorMessage: string | null = null;
+  currencyData: CurrencyData[] = currencyData;
+  date: string | null = null;
 
   constructor(
     private currencyService: CurrencyService,
-    private router: Router
+    private router: Router,
+    private datePipe: DatePipe
   ) {}
 
   /**
    * ngOnInit lifecycle hook which is executed when the component is initialized.
-   * It fetches the latest exchange rates.
+   * It fetches the latest exchange rates and loads the currency data.
    */
   ngOnInit(): void {
     this.fetchExchangeRates();
@@ -44,6 +52,7 @@ export class ExchangeRateListComponent implements OnInit {
     this.currencyService.getLatestExchangeRates().subscribe({
       next: (data: ExchangeRate[]) => {
         this.rates = data;
+        this.date = this.datePipe.transform(data[0].timestamp, 'd MMMM yyyy');
         this.errorMessage = null; // Reset error message on successful data fetch
       },
       error: (error) => {
@@ -57,6 +66,7 @@ export class ExchangeRateListComponent implements OnInit {
       },
     });
   }
+
   /**
    * Navigate to the currency page for the selected currency.
    * This method is triggered when a user click on a currency.
@@ -65,5 +75,14 @@ export class ExchangeRateListComponent implements OnInit {
    */
   goToCurrencyHistory(currency: string) {
     this.router.navigate([currency]);
+  }
+
+  /**
+   * Provides extra data for the currency
+   * @param code currency code string (e.g. USD, EUR, etc.)
+   * @returns extra data for the currency
+   */
+  getCurrencyData(code: string): CurrencyData | null {
+    return this.currencyData.find((c) => c.code === code) || null;
   }
 }
---
+++ currency-app-frontend/src/app/models/currency-data.model.ts
@@ -0,0 +1,7 @@
+export interface CurrencyData {
+  code: string;
+  symbol: string;
+  symbol_native: string;
+  name: string;
+  emoji: string;
+}
